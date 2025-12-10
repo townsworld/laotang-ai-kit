1d29a69a71483c099ceab4cdefd4f03698e609e3
Towns <jiangxinan@xd.com>
2025-12-10 09:11:11 +0800

    添加测速

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
         <div className="min-h-screen bg-gradient-to-br from-slate-950 via-slate-900 to-slate-950">
           {children}
         </div>
+        <SpeedInsights />
       </body>
     </html>
   );
